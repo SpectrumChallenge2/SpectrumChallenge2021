244d7dd87c9656460eedd042bdd3683a3b65daf6
SpectrumChallenge2 <spectrumchallenge2@gmail.com>
2021-10-01 20:38:22 +0900

    initial commit

--- Agent/keyboard_agent.py
+++ Agent/keyboard_agent.py
@@ -2,35 +2,36 @@ from utility.environment_interface import EnvironmentInterface
 import numpy as np
 
 class keyboardAgent:
-    def __init__(self, environment: EnvironmentInterface, contention_window_size, num_unit_packet):
+    def __init__(self, environment: EnvironmentInterface, num_unit_packet):
         self._env = environment
-        self._contention_window_size = contention_window_size
         self._num_unit_packet = num_unit_packet
         self._freq_channel_list = []
         self._num_freq_channel = 0
-        self._primary_channel = 0
-        self._back_off: int = np.random.randint(1, self._contention_window_size)
 
     def run(self, run_time):
         self._env.start_simulation(time_us=run_time)
         action = {'type': 'sensing'}
         self._freq_channel_list = self._env.freq_channel_list
         self._num_freq_channel = len(self._freq_channel_list)
+        print('<Action Type>')
+        print('sensing : s or S')
+        print('tx_data_packet : (0 ~ {})\n'.format(self._num_freq_channel-1))
+
         while True:
             observation = self._env.step(action=action)
             print(observation)
-            key = input('Channel number(1 ~ {}): '.format(self._num_freq_channel))
+            key = input('Action : ')
             action = self.keyboard_control(key=key)
             print(action)
 
     def keyboard_control(self, key):
         channel_list = []
         if len(key) == 1:
-            if key == 's':
+            if key == 's' or key == 'S':
                 action = {'type': 'sensing'}
             else:
                 for i in range(self._num_freq_channel):
-                    if int(key) == i + 1:
+                    if int(key) == i:
                         channel_list.append(i)
                 action = {'type': 'tx_data_packet', 'freq_channel_list': channel_list,
                           'num_unit_packet': self._num_unit_packet}
@@ -38,7 +39,7 @@ class keyboardAgent:
             key_list = key.split(',')
             int_key_list = list(map(int, key_list))
             int_key_list = np.array(int_key_list)
-            channel_list = list(int_key_list - 1)
+            channel_list = list(int_key_list)
             channel_list = [int(freq_channel) for freq_channel in channel_list]
             action = {'type': 'tx_data_packet', 'freq_channel_list': channel_list,
                       'num_unit_packet': self._num_unit_packet}
@@ -48,5 +49,5 @@ class keyboardAgent:
 if __name__ == "__main__":
     env = EnvironmentInterface()
     env.connect()
-    agent = keyboardAgent(environment=env, contention_window_size=16, num_unit_packet=1)
+    agent = keyboardAgent(environment=env, num_unit_packet=1)
     agent.run(100000)
